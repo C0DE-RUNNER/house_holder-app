ebd90da72492f87e4eaf505729ab2b4008f5f447
CODE-RUNNER <nehathakursakshi@gmail.com>
2020-02-28 12:24:58 +0530

    initial commmit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'settings.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "GoGetters App",
+      theme: new ThemeData(primarySwatch: Colors.blue),
+      home: new Settings(),
+    );
+  }
+}
+
---
+++ lib/maps.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+/*
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class Maps extends StatefulWidget {
+  @override
+  State createState() => MapPage();
+}
+
+class MapPage extends State<Maps> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Location"),
+      ),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: initialLocation,
+      ),
+    );
+  }
+  }
+*/
